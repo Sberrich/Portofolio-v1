1390001e5e33a95a9ff7e591eac9b6a742256fa2
samir berrichi <darksamir2012@gmail.com>
2024-07-10 19:40:40 +0100

    finish the Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
-
+import { TextGenerateEffect } from "./ui/TextgenerateEffect";
+import MagicButton from "./ui/MagicButton";
 function Hero() {
   return (
     <div className="pb-20 pt-36">
@@ -16,12 +17,35 @@ function Hero() {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw] " fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white text-center  dark:bg-grid-white/[0.03]
+       bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center
+         dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
       </div>
+
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col text-center jus">
-          <h2 className="upercase tracking-widest text-xs text-center">Frontend web developer</h2>
+        <div
+          className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col 
+        items-center justify-center"
+        >
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Frontend web developer
+          </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Crafting Ideas into Reality with Precision and Passion"
+          />
+          <p className="text-center md:tracking-wider text-sm md:text-lg lg:text-2xl ">
+            Hi I&apos;m Samir Berrichi, a Next.js Developer basd in Morocco{" "}
+          </p>
+          <a href="#about">
+            <MagicButton title="Show my work" />
+          </a>
         </div>
       </div>
     </div>
