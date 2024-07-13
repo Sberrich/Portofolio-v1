de3ab7ba0e6d85a7bc137d1be333d06d0f493a78
samir berrichi <darksamir2012@gmail.com>
2024-07-13 19:13:38 +0100

    Featue: HeroHighlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import {HeroHighlightDemo} from "@/components/HeroHighlightDemo";
 import {FloatingNav} from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
@@ -8,7 +9,8 @@ export default function Home() {
   <div className="relative bg-black-100 flex justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className=" w-full">
       <FloatingNav navItems={navItems}/>
-      <Hero/>
+      {/* <Hero/> */}
+      <HeroHighlightDemo/>
       <Grid/>
     </div>
   </div>
